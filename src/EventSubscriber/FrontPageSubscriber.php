d0116bb6571f91f93a24626eea7175fa9ea79bcc
Samuel <samuel@bodeboca.com>
2018-03-16 08:30:48 +0100

    Coding standards.

--- src/EventSubscriber/FrontPageSubscriber.php
+++ src/EventSubscriber/FrontPageSubscriber.php
@@ -7,16 +7,28 @@ use Symfony\Component\HttpKernel\KernelEvents;
 use Symfony\Component\HttpKernel\Event\GetResponseEvent;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
+/**
+ * Class FrontPageSubscriber.
+ *
+ * @package Drupal\front_page\EventSubscriber
+ */
 class FrontPageSubscriber implements EventSubscriberInterface {
 
+  /**
+   * Manage the logic.
+   *
+   * @param \Symfony\Component\HttpKernel\Event\GetResponseEvent $event
+   *   Managed event.
+   */
   public function initData(GetResponseEvent $event) {
     global $base_path;
 
     // Make sure front page module is not run when using cli (drush).
-    // Make sur front page module does not run when installing Drupal either.
+    // Make sure front page module does not run when installing Drupal either.
     if (PHP_SAPI === 'cli' || drupal_installation_attempted()) {
       return;
     }
+
     // Don't run when site is in maintenance mode.
     if (\Drupal::state()->get('system.maintenance_mode')) {
       return;
@@ -27,18 +39,21 @@ class FrontPageSubscriber implements EventSubscriberInterface {
       return;
     }
 
-    $front_page = null;
+    $front_page = NULL;
     $isFrontPage = \Drupal::service('path.matcher')->isFrontPage();
     if (\Drupal::config('front_page.settings')->get('enable', '') && $isFrontPage) {
 
 
       $roles = \Drupal::currentUser()->getRoles();
       $config = \Drupal::configFactory()->get('front_page.settings');
-      $current_weigth = null;
+      $current_weigth = NULL;
+
       foreach ($roles as $role) {
         $role_config = $config->get('rid_' . $role);
-        if((isset($role_config['enabled']) && $role_config['enabled'] == true) && (($role_config['weigth'] < $current_weigth) || $current_weigth === null)) {
-          //$base_path can contain a / at the end, strip to avoid double slash.
+        if ((isset($role_config['enabled']) && $role_config['enabled'] == TRUE)
+          && (($role_config['weigth'] < $current_weigth) || $current_weigth === NULL)) {
+
+          // $base_path can contain a / at the end, strip to avoid double slash.
           $path = rtrim($base_path, '/');
           $front_page = $path . $role_config['path'];
           $current_weigth = $role_config['weigth'];
@@ -49,7 +64,7 @@ class FrontPageSubscriber implements EventSubscriberInterface {
     if ($front_page) {
       $event->setResponse(new RedirectResponse($front_page));
 
-      //@todo Probably we must to remove this and manage cache by role.
+      // @todo Probably we must to remove this and manage cache by role.
       // Turn caching off for this page as it is dependant on role.
       \Drupal::service('page_cache_kill_switch')->trigger();
     }
--- src/FrontPagePathProcessor.php
+++ src/FrontPagePathProcessor.php
@@ -6,6 +6,11 @@ use Drupal\Core\PathProcessor\OutboundPathProcessorInterface;
 use Drupal\Core\Render\BubbleableMetadata;
 use Symfony\Component\HttpFoundation\Request;
 
+/**
+ * Class FrontPagePathProcessor.
+ *
+ * @package Drupal\front_page
+ */
 class FrontPagePathProcessor implements OutboundPathProcessorInterface {
 
   /**
@@ -15,7 +20,8 @@ class FrontPagePathProcessor implements OutboundPathProcessorInterface {
     if ($path == '/main') {
       $path = '';
     }
-    $config  = \Drupal::config('front_page.settings');
+
+    $config = \Drupal::config('front_page.settings');
     $new_path = $config->get('home_link_path', '');
     if (($path === '/<front>' || empty($path)) && !empty($new_path)) {
       $path = '/' . $new_path;
@@ -24,4 +30,3 @@ class FrontPagePathProcessor implements OutboundPathProcessorInterface {
   }
 
 }
-?>
