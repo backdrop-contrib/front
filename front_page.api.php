9f677ac6a9f4bb915d8eeac173c158348fd8d57a
prabeen.giri <prabeen.giri@913078.no-reply.drupal.org>
2019-05-17 13:58:42 +0200

    Issue #2312011 by prabeen.giri, fadonascimento, estoyausente: Hook to alter the front page settings data before they are redirected

---
+++ front_page.api.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * @file
+ * It has all the lists of the hook documentation
+ * that other module can implement.
+ */
+
+/**
+ * Hook to alert the front page data before setting is triggered.
+ *
+ * If there is any custom logic, this can be used
+ * to have custom redirec path.
+ *
+ * This hook gets invoked only when there is valid entry in the
+ * admin settings page. If 'skip' is set as option for any
+ * role, then this won't get invoked.
+ */
+function hook_front_page_alter(&$front_page) {
+  if ($front_page['mode'] == 'redirect'
+      && in_array('customer user', array_values($account->roles))
+      && $account->login_page == 'reports') {
+    $front_page['path'] = 'user/reports';
+  }
+}
